a7b5fae7eab468ff04191693c3c388714b1a817e
Neonchick <mishafk15@mail.ru>
2021-03-01 21:53:52 +0300

    Lab 5 re-done.

--- 05-4-1.c
+++ 05-4-1.c
@@ -16,8 +16,15 @@ int main()
   printf("Writer exist. Wating for reader.\n");
 
   if ((fd = open(name, O_WRONLY)) < 0) {
-    printf("Can\'t open FIFO for writting\n");
-    exit(-1);
+    if (mknod(name, S_IFIFO | 0666, 0) < 0) {
+      printf("Can\'t create FIFO\n");
+      exit(-1);
+    }
+
+    if ((fd = open(name, O_WRONLY)) < 0) {
+      printf("Can\'t open FIFO for writting\n");
+      exit(-1);
+    }
   }
 
   size = write(fd, "Hello, world!", 14);
